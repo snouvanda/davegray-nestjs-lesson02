17cf90e903b5f938d7e9d2196218564f2ad8b8d6
snouvanda <snouvanda@gmail.com>
2023-11-23 14:14:22 +0700

    Lesson02

---
+++ src/users/users.controller.ts
@@ -0,0 +1,39 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+
+@Controller('users')
+export class UsersController {
+  @Get() // GET /users or /users?role=value
+  findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
+    return [];
+  }
+
+  @Get(':id') // GET /users/:id
+  findOne(@Param('id') id: string) {
+    return { id };
+  }
+
+  @Post() // POST /users
+  create(@Body() user: object) {
+    return user;
+  }
+
+  @Patch(':id') // PATCH /users/:id
+  update(@Param('id') id: string, @Body() userUpdate: object) {
+    return { id, ...userUpdate };
+  }
+
+  @Delete(':id') // DELETE /users/:id
+  delete(@Param('id') id: string) {
+    return { id };
+  }
+}
